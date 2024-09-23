7bdd558e5f452d96702f02f30825e0b1d35f8b7d
unknown <abelghebz@gmail.com>
2024-09-23 18:53:55 +0100

    Add pdf

--- src/containers/Navbar.js
+++ src/containers/Navbar.js
@@ -7,7 +7,7 @@ const Navbar = () => {
   return (
     <div className="navbar">
             <p><a href="https://linkedin.com/in/abel-ghebrezadik">LinkedIn</a></p>
-            <p><a href="/assets/Abel Ghebrezadik.docx" target="_blank" rel="noopener noreferrer">Download CV</a></p>
+            <p><a href="/assets/Abel Ghebrezadik.pdf" target="_blank" rel="noopener noreferrer">Download CV</a></p>
             <p><a href="https://github.com/negus14" target="_blank" rel="noopener noreferrer">GitHub</a></p>
             <p><a href="mailto:abel_g@hotmail.co.uk">Contact via Email</a></p>
     </div>
