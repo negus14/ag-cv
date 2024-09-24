3c9d472061b3fd430d317a19af1bfcf79ef15cfc
unknown <abelghebz@gmail.com>
2024-09-24 15:04:12 +0100

    Font weight

--- src/containers/footer/Footer.js
+++ src/containers/footer/Footer.js
@@ -4,7 +4,7 @@ import './Footer.css';
 const Footer  = () => {
     return (
         <footer>
-            Created by Abel 
+            Created by Abel
             <div>
                 <img src={`${process.env.PUBLIC_URL}/logo512.png`} alt="Favicon" style={{ width: '72px', height: '72px', marginTop: '8px' }} />
             </div>
