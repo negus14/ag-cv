da17a73199970be938b8fc7dd051bdff01e41104
unknown <abelghebz@gmail.com>
2024-09-24 12:20:28 +0100

    fire emoji swithced with riley photo

--- src/containers/footer/Footer.js
+++ src/containers/footer/Footer.js
@@ -4,7 +4,10 @@ import './Footer.css';
 const Footer  = () => {
     return (
         <footer>
-            Created by Abel 🔥
+            Created by Abel 
+            <div>
+                <img src={`${process.env.PUBLIC_URL}/logo512.png`} alt="Favicon" style={{ width: '72px', height: '72px', marginTop: '8px' }} />
+            </div>
         </footer>
     );
 }
